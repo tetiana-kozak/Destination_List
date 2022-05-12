0527f86c850a6e84b2b236b4ee9ccb1362d8fd15
tetiana-kozak <kozaktanya077@gmail.com>
2022-05-12 20:32:47 +0300

    Create Destination Card in JS

---
+++ script.js
@@ -0,0 +1,65 @@
+const detailsForm = document.querySelector('#destination-form')
+detailsForm.addEventListener("submit", handleFormSubmit)
+
+function handleFormSubmit(evt){
+    evt.preventDefault()
+
+    const destName = evt.target.elements["destination-form__name"].value
+    const destLocation = evt.target.elements["destination-form__location"].value
+    const destPhoto = evt.target.elements["destination-form__photo"].value
+    const destDescr = evt.target.elements["destination-form__description"].value
+
+    for (let i = 0; i < detailsForm.length; i++) {
+        detailsForm.elements[i].value = "";
+        
+    }
+
+}
+
+function createDestCard(destFormName, destFormLocation, destFormPhoto, destFormDescr){
+    const card = document.createElement(div)
+    card.className = "destination-container__card"
+
+    const cardImg = document.createElement('img')
+    cardImg.setAttribute('alt', destFormName)
+
+    const constantPhoto = "assets/pic/01.jpg"
+    if (destFormPhoto.length === 0) {
+        cardImg.setAttribute('src', constantPhoto)
+    }else{
+        cardImg.setAttribute('src', destFormPhoto)
+    }
+    card.appendChild(cardImg)
+
+        const cardBody = document.createElement('div')
+        cardBody.className = "destination-container__card-body"
+
+            const cardText = document.createElement('div')
+            cardText.className = "destination-container__card-text"
+                
+                const cardTitle = document.createElement('h3')
+                cardTitle.innerText = destFormName
+                cardText.appendChild(cardTitle)
+
+                const cardLocation = document.createElement('h4')
+                cardLocation.innerText = destFormLocation
+                cardText.appendChild(cardLocation)
+
+                if (destFormDescr.length > 0) {        
+                    const cardDescr = document.createElement('p')
+                    cardDescr.innerText = destFormDescr        
+                    cardText.appendChild(cardDescr)
+                } 
+
+            cardBody.appendChild(cardText)
+
+                const removeBtn = document.createElement('button')
+                removeBtn.innerText = "Remove"
+    
+                removeBtn.addEventListener('click', removeDestination)
+            cardBody.appendChild(removeBtn)
+
+    card.appendChild(cardBody)
+    return card
+
+}
